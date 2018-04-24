b7c3850f668947d750bbb1067d2ee43b1259b5d0
minseok kwon <minseok.kwon@nhnent.com>
2018-04-24 20:11:03 +0900

    add bootmessaging and webflux

---
+++ bootmessaging/src/main/java/org/rvslab/chapter3/bootmessaging/BootmessagingApplication.java
@@ -0,0 +1,52 @@
+package org.rvslab.chapter3.bootmessaging;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class BootmessagingApplication implements CommandLineRunner {
+
+    @Autowired
+    Sender sender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootmessagingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        sender.send("Hello Messaging..!!!");
+    }
+}
+
+@Component
+class Sender {
+
+    @Autowired
+    RabbitMessagingTemplate template;
+
+    @Bean
+    Queue queue() {
+        return new Queue("TestQ", false);
+    }
+
+    public void send(String message) {
+        template.convertAndSend("TestQ", message);
+    }
+}
+
+@Component
+class Receiver {
+
+    @RabbitListener(queues = "TestQ")
+    public void processMessage(String content) {
+        System.out.print(content);
+    }
+}
